e531510cc3fe01dac9a1f7d8d486f3eab4bf0356
Eric <56181235+Ericbrod10@users.noreply.github.com>
2020-11-17 23:45:46 -0500

    Feature Added: Added an additional page that displayed same data but filtered

--- app/app.py
+++ app/app.py
@@ -27,6 +27,29 @@ def cities_import() -> List[Dict]:
     return result
 
 
+def cities_import2() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'citiesData'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute('SELECT * FROM tblCitiesImport WHERE fldPopulation > 10000000')
+    result = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return result
+
+
+
+
+
 @app.route('/')
 def index():
     user = {'username': 'Eric'}
@@ -40,6 +63,14 @@ def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
+
+
+@app.route('/LargeCities')
+def tryPage():
+    user = {'username': 'Eric'}
+    LargeCities_data = cities_import2()
+
+    return render_template('index.html', title='Home', user=user, cities=LargeCities_data)
 
 
 if __name__ == '__main__':
